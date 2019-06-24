e2d7038314d4a2334398dce660c08f177c672105
lumdzeehol <lumdzeehol@outlook.com>
2019-06-24 23:25:08 +0800

    no message

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
   const img = new Image();
 
   const initCanvasImg = (imgObj, {canvasW, canvasH}) => {
-    const drawObj = {w: 0, h: 0};
+    const drawObj = {w: 0, h: 0, x: 0, y: 0};
 
     if (imgPos.w > imgPos.h) {
       drawObj.w = canvasW;
@@ -15,32 +15,40 @@
       drawObj.h = canvasH;
       drawObj.w = imgPos.w * drawObj.h / imgPos.h;
     }
-    return drawObj;
+    return {...drawObj, setPos: function(x,y) {
+        this.x = x;
+        this.y = y;
+      }};
   }
 
   const imgPos = {x: 0, y: 0};
-
-  img.src = 'http://images.shejidaren.com/wp-content/uploads/2018/10/035212qNc.jpg';
-  img.onload = () => {
+  let drawObj = {};
+    img.src = 'http://images.shejidaren.com/wp-content/uploads/2018/10/035212qNc.jpg';
+    img.onload = () => {
     imgPos.w = img.width;
     imgPos.h = img.height;
 
-    const drawObj = initCanvasImg(imgPos, {canvasW, canvasH});
-    console.time();
+    drawObj = initCanvasImg(imgPos, {canvasW, canvasH});
     cxt.drawImage(img,0,0, drawObj.w, drawObj.h);
   }
 
   const startPos = {x: 0, y: 0, setPos: function(x,y) {this.x = x; this.y =y;}};
-  const canvasClient = {x: canvas.offsetLeft, y: canvas.offsetTop};
+  const endPos = {x: 0, y: 0};
+  const canvasOffset = {x: canvas.offsetLeft, y: canvas.offsetTop};
 
   canvas.onmousedown = (e) => {
-    startPos.setPos(e.clientX, e.clientY);
-    console.log(startPos);
+    startPos.setPos(e.clientX - canvasOffset.x, e.clientY - canvasOffset.y);
+    console.log(startPos.x, startPos.y);
     document.onmousemove = e => {
       cxt.clearRect(0, 0, canvasW, canvasH);
-      console.log(e);
+      cxt.drawImage(img,e.clientX - (canvasOffset.x + startPos.x) + endPos.x,e.clientY - (canvasOffset.y + startPos.y) + endPos.y,  drawObj.w, drawObj.h);
+      startPos.setPos(e.clientX - (canvasOffset.x + startPos.x) + endPos.x, e.clientY - (canvasOffset.y + startPos.y) + endPos.y);
     }
 
-    document.onmouseup = () => {document.onmousemove = null;}
+    document.onmouseup = (e) => {
+      document.onmousemove = null;
+      endPos.x = e.clientX - canvasOffset.x;
+      endPos.y = e.clientY - canvasOffset.y;
+    }
   }
 }());
