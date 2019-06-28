d61a0c918e3482b3f84b3bea383a8dbc1a8245d6
lumdzeehol <lumdzeehol@outlook.com>
2019-06-28 17:52:56 +0800

    使用本地图片

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import Canvas from './src/ld-avartar/index.js'
 // 获取一个canvas对象并初始化头像
 const cans = new Canvas('test', 180, 180)
               .initWithImg(
-                'http://images.shejidaren.com/wp-content/uploads/2018/10/035212qNc.jpg',
+                './img/img.jpeg',
                 (canvas) => {
                   canvas.draw({x: 0, y: 0});
                   canvas.drawMask();
